48ea56afe5f73a6178ad786dc14815fe0d58ff6b
Tiziano Santoro <tiziano88@gmail.com>
2017-08-06 21:25:18 +0100

    req

--- main.elm
+++ main.elm
@@ -6,6 +6,7 @@ import Http
 import Json.Decode as JD
 import Json.Encode as JE
 import Material
+import Material.Button as Button
 import Material.Card as Card
 import Material.Elevation as Elevation
 import Material.Options as Options exposing (css)
@@ -15,6 +16,8 @@ import Material.Scheme
 type alias Model =
     { serverUrl : String
     , res : Maybe SyncResponse
+    , devices : List Device
+    , mdl : Material.Model
     }
 
 
@@ -38,11 +41,11 @@ type alias SyncResponse =
 
 type alias Payload =
     { agentUserId : String
-    , devices : List Device
+    , devices : List DeviceResponse
     }
 
 
-type alias Device =
+type alias DeviceResponse =
     { id : String
     , name : Name
     , traits : List String
@@ -56,6 +59,54 @@ type alias Name =
     }
 
 
+type alias ExecuteRequest =
+    { requestId : String
+    , inputs : List ExecuteRequestInput
+    }
+
+
+type alias ExecuteRequestInput =
+    { intent : String
+    , payload : ExecuteRequestPayload
+    }
+
+
+type alias ExecuteRequestPayload =
+    { commands : List Command
+    }
+
+
+type alias Command =
+    { devices : List DeviceRequest
+    , execution : List Execution
+    }
+
+
+type alias DeviceRequest =
+    { id : String
+    }
+
+
+type alias Execution =
+    { command : String
+    , params : Params
+    }
+
+
+type alias Params =
+    { on : Bool
+    }
+
+
+type Device
+    = Light { id : String, name : String }
+
+
+toDevice : DeviceResponse -> Device
+toDevice d =
+    Light { id = d.id, name = d.name.name }
+
+
 syncResponseDecoder : JD.Decoder SyncResponse
 syncResponseDecoder =
     JD.map2 SyncResponse
@@ -65,7 +116,7 @@ syncResponseDecoder =
                 (JD.field "agentUserId" JD.string)
                 (JD.field "devices" <|
                     JD.list <|
-                        JD.map5 Device
+                        JD.map5 DeviceResponse
                             (JD.field "id" JD.string)
                             (JD.field "name" <| JD.map Name (JD.field "name" JD.string))
                             (JD.field "traits" <| JD.list JD.string)
@@ -77,8 +128,9 @@ syncResponseDecoder =
 
 type Msg
     = Nop
+    | Mdl (Material.Msg Msg)
     | Sync (Result Http.Error SyncResponse)
-    | Exec
+    | Exec ExecuteRequest
 
 
 main =
@@ -107,9 +159,10 @@ syncRequest =
         ]
 
 
-executeRequest =
+executeRequestEncoder : ExecuteRequest -> JE.Value
+executeRequestEncoder req =
     JE.object
-        [ ( "requestId", JE.string "xxx" )
+        [ ( "requestId", JE.string req.requestId )
         , ( "inputs"
           , JE.list
                 [ JE.object
@@ -149,10 +202,38 @@ executeRequest =
         ]
 
 
+onOffRequest : String -> Bool -> ExecuteRequest
+onOffRequest deviceId onOff =
+    { requestId = "11"
+    , inputs =
+        [ { intent = "action.devices.EXECUTE"
+          , payload =
+                { commands =
+                    [ { devices =
+                            [ { id = deviceId
+                              }
+                            ]
+                      , execution =
+                            [ { command = "action.devices.command.OnOff"
+                              , params =
+                                    { on = onOff
+                                    }
+                              }
+                            ]
+                      }
+                    ]
+                }
+          }
+        ]
+    }
+
+
 init : ( Model, Cmd Msg )
 init =
     ( { serverUrl = "http://localhost:1234/action"
       , res = Nothing
+      , devices = []
+      , mdl = Material.model
       }
     , Http.send Sync <| Http.post serverUrl (Http.jsonBody syncRequest) syncResponseDecoder
     )
@@ -161,34 +242,58 @@ init =
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
+        Mdl msg_ ->
+            Material.update Mdl msg_ model
+
         Sync (Ok r) ->
-            ( { model | res = Just r }, Cmd.none )
+            ( { model | res = Just r, devices = List.map toDevice r.payload.devices }, Cmd.none )
 
         Sync _ ->
             ( model, Cmd.none )
 
-        Exec ->
+        Exec req ->
             ( model
-            , Http.send Sync <| Http.post serverUrl (Http.jsonBody executeRequest) syncResponseDecoder
+            , Http.send Sync <| Http.post serverUrl (Http.jsonBody <| executeRequestEncoder req) syncResponseDecoder
             )
 
         Nop ->
             ( model, Cmd.none )
 
 
+viewDevice : Model -> Device -> Html.Html Msg
+viewDevice model d =
+    case d of
+        Light l ->
+            Card.view
+                [ css "width" "10em"
+                , Elevation.e8
+                ]
+                [ Card.title []
+                    [ Card.head [] [ Html.text l.name ]
+                    ]
+                , Card.actions []
+                    [ Button.render Mdl
+                        [ 1, 1 ]
+                        model.mdl
+                        [ Options.onClick <| Exec <| onOffRequest l.id True
+                        ]
+                        [ Html.text "on" ]
+                    , Button.render Mdl
+                        [ 1, 1 ]
+                        model.mdl
+                        [ Options.onClick <| Exec <| onOffRequest l.id False
+                        ]
+                        [ Html.text "off" ]
+                    ]
+                ]
+
+
 view : Model -> Html.Html Msg
 view model =
     Html.div []
-        [ Html.text "test"
-        , Html.button [ Html.Events.onClick Exec ] [ Html.text "execute" ]
-        , Card.view
-            [ css "height" "128px"
-            , css "width" "128px"
-            , Elevation.e8
-            ]
-            [ Card.title []
-                [ Card.head [] [ Html.text "card" ]
-                ]
-            ]
-        ]
+        ([ Html.text "test"
+           --, Button.render a
+         ]
+            ++ List.map (viewDevice model) model.devices
+        )
         |> Material.Scheme.top
