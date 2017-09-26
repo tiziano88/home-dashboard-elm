6f02050753597798186ff0bdddc41bcd183c4ccd
Tiziano Santoro <tiziano88@gmail.com>
2017-09-26 18:30:20 +0100

    remove res

--- main.elm
+++ main.elm
@@ -22,7 +22,6 @@ import Time
 
 type alias Model =
     { serverUrl : String
-    , res : Maybe SyncResponse
     , devices : Dict.Dict DeviceId Device
     , mdl : Material.Model
     }
@@ -418,7 +417,6 @@ toCssColor c =
 init : ( Model, Cmd Msg )
 init =
     ( { serverUrl = serverUrl
-      , res = Nothing
       , devices = Dict.empty
       , mdl = Material.model
       }
@@ -434,8 +432,7 @@ update msg model =
 
         Sync (Ok r) ->
             ( { model
-                | res = Just r
-                , devices =
+                | devices =
                     Dict.fromList <|
                         List.map (\d -> ( deviceId d, d )) <|
                             List.map toDevice r.payload.devices
